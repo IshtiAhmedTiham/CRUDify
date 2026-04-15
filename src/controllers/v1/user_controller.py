626737fc4ce6608e9e035d61e09b90de2cbe7f0d
ishti-ahmed <ishtiahmed191@gmail.com>
2026-04-15 21:30:19 +0600

    impliment crud

--- src/controllers/v1/user_controller.py
+++ src/controllers/v1/user_controller.py
@@ -33,32 +33,5 @@ def read_users(db : Session = Depends(get_db)):
     users = db.query(UserModel).all()
     return{"user" : users}
 
-# read specific user
-@router.get("/{id}",response_model=UserResponse,status_code=status.HTTP_200_OK)
-def read_specific_user(id:int, db:Session = Depends(get_db)):
-    try:
-        user = db.query(UserModel).filter(UserModel.id == id).order_by(UserModel.id.asc()).first()
-        return{"user" : user}
-    except Exception as error:
-        raise HTTPException(
-            status_code = status.HTTP_404_NOT_FOUND,
-            detail = f"error : {str(error)}"
-        )
-
-
-# delete user
-@router.delete("/{id}")
-def delete_user(id:int, db:Session = Depends(get_db)):
-    try:
-        user = db.query(UserModel).filter(UserModel.id == id).one()
-        db.delete(user)
-        db.commit()
-        return{"status" : "user successfully delete"}
-    except Exception as error:
-        raise HTTPException(
-            status_code = status.HTTP_404_NOT_FOUND,
-            detail = f"error : {str(error)}" 
-        )
-    
 
 
