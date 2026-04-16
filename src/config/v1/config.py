0e84d42e9935aef59320b13183bfbccf8dcf67b8
ishti-ahmed <ishtiahmed191@gmail.com>
2026-04-16 22:06:07 +0600

    setup main file

--- src/config/v1/config.py
+++
@@ -1,6 +0,0 @@
-from pydantic_settings import BaseSettings
-
-class Configuration(BaseSettings):
-    @property
-    def database_url(self):
-        return "sqlite:///app.db"
--- src/routes/v1/router.py
+++
@@ -1,7 +0,0 @@
-from fastapi import APIRouter
-from src.controllers.v1 import user_controller
-
-router = APIRouter()
-
-router.include_router(user_controller.router,prefix="/user")
-
