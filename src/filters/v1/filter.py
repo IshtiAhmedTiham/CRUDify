9d1a39ed7dc7f3bc6b06a5a5c4d97e1413a59a89
ishti-ahmed <ishtiahmed191@gmail.com>
2026-04-24 08:43:19 +0600

    add virtual enviourment

--- src/filters/v1/filter.py
+++
@@ -1,6 +0,0 @@
-from pydantic import BaseModel,Field,EmailStr
-from typing import Optional
-
-class UserFilters(BaseModel):
-    name : Optional[str] = Field(None,min_length=1)
-    email : Optional[EmailStr] = None
--- src/routes/v1/router.py
+++
@@ -1,6 +0,0 @@
-from fastapi import APIRouter
-from src.controllers.api.v1 import user_controller
-
-router = APIRouter()
-
-router.include_router(user_controller.router,prefix="/user")
