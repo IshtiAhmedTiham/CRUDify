a552e8da68c0bfea8e958e35932667b475f47c78
ishti-ahmed <ishtiahmed191@gmail.com>
2026-05-21 21:42:56 +0600

    start new

--- src/routers/v1/postgresql/postgresql_router.py
+++
@@ -1,7 +0,0 @@
-from fastapi import APIRouter
-from src.controllers.api.v1.postgresql import user_contoller
-
-
-router = APIRouter()
-
-router.include_router(user_contoller.router, prefix="/api/v1/user")
--- src/routers/v1/router.py
+++
@@ -1,8 +0,0 @@
-from fastapi import APIRouter
-from .sqlite import sqlite_router
-from .postgresql import postgresql_router
-
-router = APIRouter()
-
-router.include_router(sqlite_router.router, prefix="/sqlite")
-router.include_router(postgresql_router.router, prefix="/postgresql")
